78013b69cbd04a2e181ce91421ea3d26ebcafa66
Eugene <yukovich01@mail.ru>
2022-07-06 21:05:58 +0300

    Example of using the reflect-metadata library

---
+++ typescript/app.ts
@@ -0,0 +1,63 @@
+import 'reflect-metadata';
+
+const POSITIVE_METADATA_KEY = Symbol('POSITIVE_METADATA_KEY')
+
+interface IUserService {
+    getUsersInDatabase(): number;
+}
+
+class UserService implements IUserService {
+    private _users: number;
+
+    getUsersInDatabase(): number {
+        return this._users;
+    }
+
+    @Validate()
+    setUsersInDatabase(@Positive() num: number): void {
+        this._users = num;
+    }
+}
+
+
+function Positive() {
+    return (
+        target: Object,
+        propertyKey: string | symbol,
+        parameterIndex: number 
+    ) => {
+        console.log(Reflect.getOwnMetadata('design:type', target, propertyKey));
+        console.log(Reflect.getOwnMetadata('design:paramtypes', target, propertyKey));
+        console.log(Reflect.getOwnMetadata('design:returntype', target, propertyKey));
+        let existParams: number[] = Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, propertyKey) || [];
+        existParams.push(parameterIndex);
+        Reflect.defineMetadata(POSITIVE_METADATA_KEY, existParams, target, propertyKey);
+    }
+}
+
+function Validate() {
+    return (
+        target: Object,
+        propertyKey: string | symbol,
+        descriptor: TypedPropertyDescriptor<(...args: any[]) =>  any>
+    ) => {
+        let method = descriptor.value;
+        descriptor.value = function (...args: any) {
+            let positiveParams: number[] = Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, propertyKey);
+            if (positiveParams) {
+                for (let index of positiveParams) {
+                    if (args[index] < 0) {
+                        throw new Error('The number must be greater than 0')
+                    }
+                }
+            }
+            return method?.apply(target, args);
+        }
+    }
+}
+
+const userService = new UserService();
+console.log(userService.setUsersInDatabase(10));
+console.log(userService.setUsersInDatabase(15));
+console.log(userService.getUsersInDatabase());
+console.log(userService.setUsersInDatabase(-1));
